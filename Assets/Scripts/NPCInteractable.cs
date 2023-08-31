3a96f720608dbcc631634f8459bdfba3564f9008
Tabea Bolliger <tabea.bolliger@emea.merkleinc.com>
2023-08-31 10:24:19 +0200

    npc

---
+++ Assets/Scripts/NPCInteractable.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCInteractable : MonoBehaviour
+{
+    public void Interact(){
+      Debug.Log("Interact!");
+    }
+}
---
+++ Assets/Scripts/PlayerInteract.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInteract : MonoBehaviour
+{
+
+    private void Update(){
+      if (Input.GetKeyDown(KeyCode.E)) {
+        float interactRange = 2f;
+        Collider[] colliderArray = Physics.OverlapSphere(transform.position, interactRange);
+        foreach (Collider collider in colliderArray){
+          if (collider.TryGetComponent(out NPCInteractable npcInteractable)) {
+            npcInteractable.Interact();
+
+          }
+        }
+      }
+    }
+}
